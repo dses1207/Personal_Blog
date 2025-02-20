5289e398190efa1d0374b8d0e1213b76dfb18207
Roy Li <dses1207@gmail.com>
2025-02-20 15:55:39 +0800

    Update: v2

---
+++ content/posts/funny.md
@@ -0,0 +1,11 @@
+---
+title: 劉庭輔屁眼很臭
+description: 真的很臭
+slug: sample-post
+date: 2025-02-20
+type: Post
+---
+
+## This is my first sample post
+
+Congratulations! You have created your first post.
---
+++ contentlayer.config.ts
@@ -0,0 +1,37 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+// 定義一個 Post 文件類型
+export const Post = defineDocumentType(() => ({
+    name: 'Post',
+    filePathPattern: `content/posts/**/*.md`,
+    fields: {
+      title: {
+        type: 'string',
+        required: true,
+      },
+      description: {
+        type: 'string',
+        required: true,
+      },
+      slug: {
+        type: 'string',
+        required: true,
+      },
+      date: {
+        type: 'date',
+        required: true,
+      },
+    },
+    computedFields: {
+      path: {
+        type: 'string',
+        resolve: (post) => `/posts/${post.slug}`,
+      },
+    },
+  }));
+
+// 建立 Contentlayer 資料來源設定
+export default makeSource({
+  contentDirPath: 'content', // 內容檔案放置的資料夾
+  documentTypes: [Post],
+})
